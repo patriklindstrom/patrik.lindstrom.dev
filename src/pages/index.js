381cc585cfaa1dce9295c3b95803fb990cfe1508
Patrik Lindström <patrik.lindstrom@lcube.se>
2019-06-18 13:08:12 +0200

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi fellow Developers</h1>
+    <p>Welcome to patrik.lindstrom.dev blog.</p>
+    <p>First proof of concept site made from my iPad Pro.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
